95fe1fef85a93cecf1ea0d16a723711a71599ed2
dloyolar <diego.loyola@continuumhq.com>
2022-08-06 11:34:06 -0400

    start react native app

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+      <Icon name="rocket" size={30} color="#900" />
+    </View>
+  );
+};
+
+export default App;
